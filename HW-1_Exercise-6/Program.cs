6eec884f1e0c0c1ca9101ad6f61a8315428b9f35
Maximus749 <maximus749@gmail.com>
2022-11-25 17:47:53 +0300

    Add HW-1_Exercise-6. Решил задачу № 6.

---
+++ HW-1_Exercise-6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Insert number please: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0){
+    Console.WriteLine("Число " + number1 + " - четное.");
+}
+else{
+    Console.WriteLine("Число " + number1 + " - нечетное.");
+}
