ceecb4bd3438ed207c4a191ecac0fdd51a301f2e
Maximus749 <maximus749@gmail.com>
2022-12-08 14:27:49 +0300

    Сделал задание 34 семинара 5

---
+++ HW-5_Exercise-34/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray(int lenght)
+{
+    int[] arr = new int[lenght];
+    for(int i = 0; i < lenght; i++){
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintAndSelect(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++){
+        Console.Write($"{arr[i]} ");
+    }
+
+    Console.Write(" -> ");
+
+    int count = 0;
+    for (int j = 0; j < arr.Length; j++){
+        if (arr[j] % 2 == 0) count += 1;
+    }
+    Console.WriteLine(count);
+}
+
+int[] numbers = CreateArray(9);
+PrintAndSelect(numbers);
