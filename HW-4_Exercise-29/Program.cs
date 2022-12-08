3aabc64f0ecfd7d2a0b7bdb28bd2ae72db870592
Maximus749 <maximus749@gmail.com>
2022-12-08 13:45:29 +0300

    Сделал задание 28 семинара 4

---
+++ HW-4_Exercise-29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArray(int lenght)
+{
+    int[] name = new int[lenght];
+    for (int i = 0; i < lenght; i++){
+        name[i] = new Random().Next(0, 100);
+    }
+    return name;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] numbers = CreateArray(8);
+PrintArray(numbers);
