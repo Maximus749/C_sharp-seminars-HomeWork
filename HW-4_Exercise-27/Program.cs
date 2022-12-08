1f2eb86220125e6cf3886c3147a36ed262240faf
Maximus749 <maximus749@gmail.com>
2022-12-08 11:55:54 +0300

    Решил задачу 27 семинара 4

---
+++ HW-4_Exercise-27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Summ(string insert)
+{
+    int lenght = insert.Length;
+    int number = Convert.ToInt32(insert);
+    int sum = 0;
+    for (int i = 0; i < lenght; i ++){
+        if (number > 9){
+            sum = sum + number % 10;
+        }
+        else sum = sum + number;
+        number = number / 10;
+
+    }
+    return sum;
+}
+Console.Write("Введите число: ");
+string newString = Console.ReadLine();
+Console.WriteLine(Summ(newString));
